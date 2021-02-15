3ebd9ff22c9cfb5c6449882907cc0c6e2e4c011f
Mochikibi <masaya85710719@icloud.com>
2021-02-15 16:49:01 +0900

    ユーザーモデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   validates :last_name_kana
   end
 
+  validates :birth_date, presence: true
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nick_name {"ササキさん"}
+    last_name {"佐々木"}
+    first_name {"雅也"}
+    email {Faker::Internet.free_email}
+    password = "Masaya85"
+    password {password}
+    password_confirmation {password}
+    last_name_kana {"ササキ"}
+    first_name_kana {"マサヤ"}
+    birth_date {"1999-07-19"}
+  end
+end
